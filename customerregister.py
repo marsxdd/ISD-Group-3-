e2674d00437ec2f8c52bb175cefdd8cd11e78f4d
Marsha D'Andrade <145869849+marsxdd@users.noreply.github.com>
2023-12-04 01:34:40 -0400

    Update and rename customer_regi.py to customerregister.py

--- customerregister.py
+++ customerregister.py
@@ -1,15 +1,18 @@
-#Customer Register page
+# Customer Register page
 import tkinter as tk
 from tkinter import messagebox
-#messagebox gives pop ups!(?)
+# messagebox gives pop ups!(?)
+import subprocess
 
-#main window
+
+# main window
 window = tk.Tk()
 window.title("SwiftRide - Customer Register")
 window.geometry("300x300")
 window.config(bg="Orange")
 
-#functionality
+
+# functionality
 def register():
     first_name = fname_entry.get()
     last_name = lname_entry.get()
@@ -20,76 +23,83 @@ def register():
     password = password_entry.get()
     confirm_password = password_con_entry.get()
 
-#Password functionality  
-    if password != confirm_password: #!= Comparison op
-        messagebox.showerror("Error", "Passwords do not match! Try Again")
+    #password func
+    if password != confirm_password:  # != Comparison op
+        messagebox.showerror(title="Error!")
     else:
-        messagebox.showinfo("Registration", "Registration successful! :D")
+        messagebox.showinfo(title="Success!")
 
 
-
-# First Name
+#first name
 fname = tk.Label(text="First Name:", bg="orange")
 fname.pack()
 fname_entry = tk.Entry()
 fname_entry.pack()
 
-# Last Name
+#last name
 lname = tk.Label(text="Last Name:", bg="orange")
 lname.pack()
 lname_entry = tk.Entry()
 lname_entry.pack()
 
-# Address
+#address
 address = tk.Label(text="Address:", bg="orange")
 address.pack()
 address_entry = tk.Entry()
 address_entry.pack()
 
-#Email
+#email
 email = tk.Label(text="Email:", bg="orange")
 email.pack()
 email_entry = tk.Entry()
 email_entry.pack()
 
-#Phone Number
+#phone number
 pnumber = tk.Label(text="Phone Number:", bg="orange")
 pnumber.pack()
 pnumber_entry = tk.Entry()
 pnumber_entry.pack()
 
-#Payment Method
+#payment
 payment_method = tk.Label(text="Payment Method:", bg="orange")
 payment_method.pack()
 payment_method_entry = tk.Entry()
 payment_method_entry.pack()
 
-#Password
+#password
 password = tk.Label(text="Password:", bg="orange")
 password.pack()
 password_entry = tk.Entry(show="*")
 password_entry.pack()
 
-#Confirm Password
+#password
 password_confirm = tk.Label(text="Confirm Password:", bg="orange")
 password_confirm.pack()
 password_con_entry = tk.Entry(show="*")
 password_con_entry.pack()
 
-#Register Button
+#egibtn
 register_btn = tk.Button(text="Register", command=register, activebackground="green")
 register_btn.pack()
 
-# Already have an account?
-login = tk.Label(text="Already have an account?" , bg="orange")
+#haveacc
+login = tk.Label(text="Already have an account?", bg="orange")
 login.pack()
-login_arrows = tk.Label(text="↓↓↓" , bg="orange")
+login_arrows = tk.Label(text="↓↓↓", bg="orange")
 login_arrows.pack()
-"""LOG IN BUTTON HERE"""
 
-"""
-↑↑↑↑↑↑↑↑Need to make button up here to get back to login page(?)↑↑↑↑
-"""
+#login btn
+def open_file():
+    subprocess.Popen(['python', 'login_page.py'])
+login_btn = tk.Button(text="Login", command=open_file)
+login_btn.pack()
 
-#Dont forget this
+#back btn
+def open_file():
+    subprocess.Popen(['python', 'user_selection_register.py'])
+
+back_btn = tk.Button(text="Back", command=open_file)
+back_btn.pack(side= tk.BOTTOM, pady=5)
+
+# Dont forget this
 window.mainloop()
