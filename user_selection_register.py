d4ddeebf63bb308935bb37f919317a612381fd76
Marsha D'Andrade <145869849+marsxdd@users.noreply.github.com>
2023-12-04 01:35:41 -0400

    Create user_selection_register.py

---
+++ user_selection_register.py
@@ -0,0 +1,40 @@
+from tkinter import *
+import tkinter as tk
+import subprocess
+
+# main window
+window = Tk()
+window.title("SwiftRide - Register Selection")
+window.geometry("300x200")
+window.config(bg="Orange")
+
+#Label
+label = Label(text="Who's Registering?",activebackground="red")
+label.pack()
+
+#Button funct
+#CUSTOMER
+def open_file():
+    subprocess.Popen(['python', 'customerregister.py'])
+
+login3_btn = tk.Button(text="Customer", command=open_file)
+login3_btn.pack()
+
+#DRIVER
+def open_file():
+    subprocess.Popen(['python', 'driverregister.py'])
+
+login4_btn = tk.Button(text="Driver", command=open_file)
+login4_btn.pack()
+
+#back
+def open_file():
+    subprocess.Popen(['python', 'user_selection_login.py'])
+
+back_btn = tk.Button(text="Back", command=open_file)
+back_btn.pack(side= tk.BOTTOM, pady=5)
+
+
+
+#IMPORTANT THING
+window.mainloop()
