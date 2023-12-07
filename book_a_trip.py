d72dc58db6aac2f7daba501783580c563331d95d
Marsha D'Andrade <145869849+marsxdd@users.noreply.github.com>
2023-12-07 15:32:05 -0400

    Update book_a_trip.py
    rehauled page to show receipt

--- book_a_trip.py
+++ book_a_trip.py
@@ -1,15 +1,9 @@
-from tkinter import *
+import tkinter as tk
+import random
 import subprocess
-
-#main window
-window = Tk()
-window.title("SwiftRide - Book a Trip")
-window.geometry("300x200")
-window.config(bg="Orange")
-
 def confirm_trip():
-    booking_id = booking_id_entry.get()
     first_name = first_name_entry.get()
+    booking_id = random.randint(1000, 9999)  # Randomized booking ID
     last_name = last_name_entry.get()
     destination = destination_entry.get()
     pickup_address = pickup_address_entry.get()
@@ -17,69 +11,88 @@ def confirm_trip():
     booking_date = booking_date_entry.get()
     payment_method = payment_method_entry.get()
 
-#booking id
-booking_id = Label(text="Booking ID:", bg="orange")
-booking_id.pack()
-booking_id_entry = Entry()
-booking_id_entry.pack()
+#receipt
+    receipt_window = tk.Toplevel(bg="orange")
 
-#first name
-first_name = Label(text="First Name:", bg="orange")
-first_name.pack()
-first_name_entry = Entry()
+#booking information
+    booking_id_label = tk.Label(receipt_window, bg="orange", text="Booking ID: " + str(booking_id))
+    booking_id_label.pack()
+
+    name_label = tk.Label(receipt_window, bg="orange", text="Name: " + first_name + " " + last_name)
+    name_label.pack()
+
+    destination_label = tk.Label(receipt_window, bg="orange", text="Destination: " + destination)
+    destination_label.pack()
+
+    pickup_label = tk.Label(receipt_window, bg="orange", text="Pick-up Address: " + pickup_address)
+    pickup_label.pack()
+
+    pickup_time_label = tk.Label(receipt_window, bg="orange", text="Pick-up Time: " + pickup_time)
+    pickup_time_label.pack()
+
+    booking_date_label = tk.Label(receipt_window, bg="orange", text="Booking Date: " + booking_date)
+    booking_date_label.pack()
+
+    payment_method_label = tk.Label(receipt_window, bg="orange", text="Payment Method: " + payment_method)
+    payment_method_label.pack()
+
+#btn to close window
+    close_button = tk.Button(receipt_window, text="Close", command=receipt_window.destroy)
+    close_button.pack()
+
+#main window
+window = tk.Tk()
+window.title("SwiftRide - Book a Trip")
+window.geometry("300x200")
+window.config(bg="Orange")
+
+#entry fields for booking information
+first_name_label = tk.Label(text="First Name:", bg="orange")
+first_name_label.pack()
+first_name_entry = tk.Entry()
 first_name_entry.pack()
 
-#last name
-last_name = Label(text="Last Name:", bg="orange")
-last_name.pack()
-last_name_entry = Entry()
+last_name_label = tk.Label(text="Last Name:", bg="orange")
+last_name_label.pack()
+
+last_name_entry = tk.Entry()
 last_name_entry.pack()
 
-#destination
-destination = Label(text="Destination:", bg="orange")
-destination.pack()
-destination_entry = Entry()
+destination_label = tk.Label(text="Destination:", bg="orange")
+destination_label.pack()
+destination_entry = tk.Entry()
 destination_entry.pack()
 
-#pick up ad
-pickup_address = Label(text="Pickup Address:", bg="orange")
-pickup_address.pack()
-pickup_address_entry = Entry()
+pickup_address_label = tk.Label(text="Pick-up Address:", bg="orange")
+pickup_address_label.pack()
+pickup_address_entry = tk.Entry()
 pickup_address_entry.pack()
 
-#pick up time
-pickup_time = Label(text="Pickup Time:", bg="orange")
-pickup_time.pack()
-pickup_time_entry = Entry()
+pickup_time_label = tk.Label(text="Pick-up Time:", bg="orange")
+pickup_time_label.pack()
+pickup_time_entry = tk.Entry()
 pickup_time_entry.pack()
 
-#date
-booking_date = Label(text="Booking Date:", bg="orange")
-booking_date.pack()
-booking_date_entry = Entry()
+booking_date_label = tk.Label(text="Booking Date:", bg="orange")
+booking_date_label.pack()
+booking_date_entry = tk.Entry()
 booking_date_entry.pack()
 
-#payment
-payment_method = Label(text="Payment Method:")
-payment_method.pack()
-payment_method_entry = Entry()
+payment_method_label = tk.Label(text="Payment Method:", bg="orange")
+payment_method_label.pack()
+payment_method_entry = tk.Entry()
 payment_method_entry.pack()
 
-#confir
-confirm_btn = Button(text="Confirm Trip", command=confirm_trip)
-confirm_btn.pack()
+#confirm the trip
+confirm_button = tk.Button(text="Confirm Trip", command=confirm_trip)
+confirm_button.pack()
 
-#view booking
-view_booking_btn = Button(text="View Booking")
-view_booking_btn.pack()
-
-#back btn
 def open_file():
     subprocess.Popen(['python', 'user_selection_login.py'])
 
-back_btn = Button(text="Back", command=open_file)
-back_btn.pack(side= BOTTOM, pady=5)
+back_btn = tk.Button(text="Back", command=open_file)
+back_btn.pack(pady=5)
 
 
-#da loop
+#IMPORTANT
 window.mainloop()
